317b74d3ddb453466d2c44334b9759148f19743e
xygodcyx <1323943635@qq.com>
2023-01-11 20:28:43 +0800

    修复bug
    食物会出界的bug

--- src/moduls/Food.ts
+++ src/moduls/Food.ts
@@ -1,9 +1,11 @@
 //定义食物类
 class Food {
   element: HTMLElement;
+  stage: HTMLElement;
   constructor() {
     this.element = document.getElementById("food")!;
     this.ChangePosition();
+    this.stage = document.getElementById("stage")!;
   }
   get X() {
     return this.element.offsetLeft;
@@ -14,7 +16,10 @@ class Food {
   ChangePosition() {
     let left = Math.round(Math.random() * 29) * 10;
     let top = Math.round(Math.random() * 29) * 10;
-    if (left <= 280 && top <= 280) {
+    if (
+      left <= this.stage.offsetWidth - 10 &&
+      top <= this.stage.offsetHeight - 10
+    ) {
       this.element.style.left = left + "px";
       this.element.style.top = top + "px";
     }
