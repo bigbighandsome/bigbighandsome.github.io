96429b6442b45948a03daa3b94e7f30cd63e6e6c
xygodcyx <xygodcyx@users.noreply.github.com>
2024-05-24 10:24:25 +0000

    deploy: b761be9bd09e7bf61ee2e6c6d5fb3c63f7af8f18

---
+++ app.js
@@ -0,0 +1,39 @@
+const os = require('os');
+const app = require('express')();
+
+// 获取当前主机的网络接口信息
+const networkInterfaces = os.networkInterfaces();
+
+// 过滤出 IPv4 地址并排除 localhost
+const ipv4Addresses = Object.values(networkInterfaces)
+	.flat()
+	.filter((info) => info.family === 'IPv4' && info.internal === false);
+
+const port = 80;
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Headers', '*');
+	res.header('Access-Control-Allow-Origin', '*');
+	if (req.method === 'OPTIONS') {
+		res.sendStatus(200);
+	} else {
+		next();
+	}
+});
+
+app.get('/', (req, res) => {
+	res.send('hello world');
+});
+
+// 获取第一个可用的 IPv4 地址和端口号
+if (ipv4Addresses.length > 0) {
+	const { address } = ipv4Addresses[0];
+	const port = process.env.PORT || 3000; // 或者您使用的端口号
+
+	const currentAddress = `http://${address}:${port}`;
+	app.listen(port, () => {
+		console.log('当前运行地址:', currentAddress);
+	});
+} else {
+	console.log('无法获取当前运行地址');
+}
--- create.js
+++ create.js
@@ -1,25 +1,3 @@
-// const app = require('express')();
-
-// const port = 80;
-
-// app.use((req, res, next) => {
-// 	res.header('Access-Control-Allow-Headers', '*');
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	if (req.method === 'OPTIONS') {
-// 		res.sendStatus(200);
-// 	} else {
-// 		next();
-// 	}
-// });
-
-// app.get('/', (req, res) => {
-// 	res.send('hello world');
-// });
-
-// app.listen(port, () => {
-// 	console.log(`server is running on localhost:${port}`);
-// });
-
 const fs = require('fs');
 const path = require('path');
 
